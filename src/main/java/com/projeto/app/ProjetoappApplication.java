9e932566dc76df237f10a2ab8edf7329c9a6bd7d
Lucas Cabral <lcabralramos@gmail.com>
2020-08-11 00:58:31 -0300

    Projeto criado

---
+++ src/main/java/com/projeto/app/ProjetoappApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoappApplication.class, args);
+	}
+
+}
